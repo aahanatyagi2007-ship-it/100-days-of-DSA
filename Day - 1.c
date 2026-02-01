72a7747f4fb4a77ac109155f1395eefbe4ae4d05
aahanatyagi2007-ship-it <aahanatyagi2007@gmail.com>
2026-02-01 16:35:07 +0530

    question - 1

--- Day - 1.c
+++ Day - 1.c
@@ -1,7 +1,7 @@
 //Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
 #include <stdio.h>
 int main() {
-  int arr[10] = {10,20,30,40,50};
+int arr[10] = {10,20,30,40,50};
 int size = 5;
 int pos = 2;
 int value = 25;
